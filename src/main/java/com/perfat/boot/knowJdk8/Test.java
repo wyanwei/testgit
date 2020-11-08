46a742340c0e0a419563f5c877f07b28e4fd0dda
wangyw <1186125558@qq.com>
2020-11-08 08:03:06 +0800

    测试提交

---
+++ src/main/java/com/perfat/boot/knowJdk8/Test.java
@@ -0,0 +1,12 @@
+/*
+ * Chsi
+ * Created on 2020-10-31
+ */
+package com.perfat.boot.knowJdk8;
+
+/**
+ * @author wangyw <a href="mailto:wangyw@chsi.com.cn">WangYanWei</a>
+ * @version $Id$
+ */
+public class Test {
+}
