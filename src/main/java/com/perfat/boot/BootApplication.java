19f905dd83887662dc11d1563d637c0012e34d9e
wangyw <wangyw@chsi.com.cn>
2020-11-12 09:12:30 +0800

    develop提交测试

--- src/main/java/com/perfat/boot/BootApplication.java
+++ src/main/java/com/perfat/boot/BootApplication.java
@@ -20,7 +20,7 @@ public class BootApplication {
     public static void main(String[] args) {
 
         //测试git 测试提交测--主干提交
-        //dev-测试提交
+        //dev-测试提交-de1
         SpringApplication.run(BootApplication.class, args);
     }
 
