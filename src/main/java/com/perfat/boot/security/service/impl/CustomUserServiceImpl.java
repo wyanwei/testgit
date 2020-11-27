1cf1b08a03f9d950939bf1869a1752af4714ce7b
wangyw <wangyw@chsi.com.cn>
2020-11-27 14:56:58 +0800

    代码调整dev-4

--- src/main/java/com/perfat/boot/security/service/impl/CustomUserServiceImpl.java
+++ src/main/java/com/perfat/boot/security/service/impl/CustomUserServiceImpl.java
@@ -33,18 +33,18 @@ public class CustomUserServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        if (StringUtils.isBlank(userName)) {
+        if ( StringUtils.isBlank(userName) ) {
             return null;
         }
         UserInfoVo userInfoVo = securityService.getUserInfoByUsername(userName);
-        if (null == userInfoVo) {
+        if ( null == userInfoVo ) {
             return null;
         }
-        if (null == userInfoVo.getRoleVoList() || userInfoVo.getRoleVoList().isEmpty()) {
+        if ( null == userInfoVo.getRoleVoList() || userInfoVo.getRoleVoList().isEmpty() ) {
             return new User(userInfoVo.getUserName(), userInfoVo.getPassword(), null);
         }
         List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
-        for (UserRoleVo userRoleVo : userInfoVo.getRoleVoList()) {
+        for ( UserRoleVo userRoleVo : userInfoVo.getRoleVoList() ) {
             authorityList.add(new SimpleGrantedAuthority(userRoleVo.getName()));
         }
         return new User(userInfoVo.getUserName(), userInfoVo.getPassword(), authorityList);
