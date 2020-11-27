79943501828b2523df10d2736efe44366f97edee
wangyw <wangyw@chsi.com.cn>
2020-11-27 15:02:05 +0800

    develop提交

--- src/main/java/com/perfat/boot/knowJdk8/SupplierTest.java
+++ src/main/java/com/perfat/boot/knowJdk8/SupplierTest.java
@@ -13,7 +13,7 @@ import java.util.function.Supplier;
 public class SupplierTest {
     public static void main(String[] args) {
         //创建Supplier容器，声明为SupplierTest类型，此时并不会调用对象的构造方法，即不会创建对象
-        Supplier<SupplierTest> test = SupplierTest::new;
+        Supplier<SupplierTest> test = SupplierTest ::new;
 
         //get的时候才会创建对象
         test.get().say(() -> test.get());
