d677da196aa9f3ed16392a9446e54c3d779d22fa
wangyw <wangyw@chsi.com.cn>
2020-11-27 15:26:45 +0800

    dev-6

--- src/main/java/com/perfat/boot/memcache/util/Test.java
+++ src/main/java/com/perfat/boot/memcache/util/Test.java
@@ -16,22 +16,22 @@ public class Test {
      * volatile 的好处，禁止指令重排，可见性
      */
     private volatile static Test test;
-    
+
     public static Test getInstance() {
         if ( null == test ) {
-            synchronized (Test.class){
+            synchronized ( Test.class ) {
                 if ( null == test ) {
                     //正确流程：分配内容、内存中创建对象，变量指向刚分配的内容地址(分配地址就会test != null)，由于指向重排，可以造成 内容中创建对象与变量指向分配的内容地址
-                    test =  new Test();
+                    test = new Test();
                 }
             }
         }
         return null;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Calendar nowDate = Calendar.getInstance();
-        nowDate.add(Calendar.HOUR_OF_DAY,-2);
+        nowDate.add(Calendar.HOUR_OF_DAY, -2);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println(sdf.format(nowDate.getTime()));
 
